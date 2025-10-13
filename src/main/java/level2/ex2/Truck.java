1dba9c853106d5bb21d91fc53e24768b7471bb16
David Rey <tintoelculo@gmail.com>
2025-10-13 13:10:06 +0200

    feat: level 2 completed

--- src/main/java/level2/ex2/Truck.java
+++
@@ -1,9 +0,0 @@
-package level2.ex2;
-
-public class Truck {
-    String name;
-
-    public Truck(String name) {
-        this.name = name;
-    }
-}
--- src/test/java/level1/ex2/Model/CalculateIdTest.java
+++ src/test/java/level1/ex2/Model/CalculateIdTest.java
@@ -1,6 +1,5 @@
 package level1.ex2.Model;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
