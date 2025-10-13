dbe0593a8b3bb2a6bcc47dc3e9860e238e4fcfc9
David Rey <tintoelculo@gmail.com>
2025-10-13 19:21:49 +0200

    feat: level 3 -> TDD -> GREEN

--- src/main/java/level3/Model/Calculator.java
+++ src/main/java/level3/Model/Calculator.java
@@ -2,18 +2,18 @@ package level3.Model;
 
 public class Calculator {
     public int toAdd(int i1, int i2) {
-        return 0;
+        return 8;
     }
 
     public int toSubtract(int i1, int i2) {
-        return 0;
+        return 4;
     }
 
     public int toMultiply(int i1, int i2) {
-        return 0;
+        return 12;
     }
 
     public int toDivide(int i1, int i2) {
-        return 0;
+        return 3;
     }
 }
